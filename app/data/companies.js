aed5eb01208b5fe3b74f476e26d428a572ed6c55
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-02 13:59:14 -0700

    Database modifications

--- app/data/companies.js
+++ app/data/companies.js
@@ -2,79 +2,79 @@ var companyArray = [
     {
         name: "Google", 
         photo: "google.com", 
-        scores: [2, 5, 5, 1, 1, 3, 4, 2, 5, 2]
+        scores: [2, 5, 5, 1, 1, 3, 4, 2, 5, 3]
     }, 
     {
         name: "Facebook", 
         photo: "facebook.com", 
-        scores: [2, 5, 5, 3, 1, 3, 4, 1, 4, 2]
+        scores: [2, 5, 5, 3, 1, 3, 4, 1, 5, 3]
     }, 
     {
         name: "Apple",
         photo: "apple.com", 
-        scores: [1, 5, 5, 2, 1, 3, 3, 3, 3, 3] 
+        scores: [1, 5, 5, 2, 1, 3, 3, 3, 5, 3] 
 
     }, 
     {
         name: "Amazon", 
         photo: "amazon.com", 
-        scores: [1, 5, 5, 3, 1, 3, 3, 3, 3, 3]
+        scores: [1, 5, 5, 3, 1, 4, 3, 2, 4, 2]
     }, 
     {
         name: "Microsoft", 
         photo: "microsoft.com", 
-        scores: [2, 5, 5, 2, 2, 3, 3, 3, 3, 3]
+        scores: [2, 5, 5, 2, 2, 2, 3, 3, 4, 3]
     }, 
     {
         name: "ACLU", 
         photo: "aclu.com", 
-        scores: [1, 5, 2, 2, 5, 3, 3, 3, 3, 3]
+        scores: [1, 5, 2, 2, 5, 1, 2, 4, 4, 4]
     }, 
     {
         name: "Electronic Frontier Foundation", 
         photo: "eff.org", 
-        scores: [1, 3, 5, 2, 5, 3, 3, 3, 3, 3]
+        scores: [1, 3, 5, 2, 5, 1, 2, 5, 3, 4]
     }, 
     {
         name: "Kiva", 
         photo: "kiva.org", 
-        scores: [3, 3, 4, 5, 5, 3, 3, 3, 3, 3]
+        scores: [3, 3, 4, 5, 5, 1, 3, 5, 2, 3]
     }, 
     {
         name: "Earth Justice", 
         photo: "earthjustice.org", 
-        scores: [1, 3, 3, 2, 5, 3, 3, 3, 3, 3] 
+        scores: [1, 3, 3, 2, 5, 1, 2, 5, 4, 4] 
     }, 
     {
         name: "Brex", 
         photo: "brex.com", 
-        scores: [5, 3, 5, 5, 1, 3, 3, 3, 3, 3]
+        scores: [5, 3, 5, 5, 1, 5, 4, 4, 4, 2]
     }, 
     {
         name: "Superhuman", 
         photo: "superhuman.com", 
-        scores: [5, 2, 5, 3, 1, 3, 3, 3, 3, 3]
+        scores: [5, 2, 5, 3, 1, 4, 4, 4, 3, 2]
     }, 
     {
         name: "McKinsey", 
         photo: "mckinsey.com", 
-        scores: [2, 5, 3, 5, 1, 3, 3, 3, 3, 3]
+        scores: [2, 5, 3, 5, 1, 1, 2, 3, 4, 2]
     }, 
     {
         name: "Goldman Sachs", 
         photo: "goldmansachs.com", 
-        scores: [1, 5, 3, 5, 1, 3, 3, 3, 3, 3]
+        scores: [1, 5, 3, 5, 1, 1, 2, 2, 3, 1]
     }, 
     {
         name: "Morgan Stanley", 
         photo: "morganstanley.com", 
-        scores: [1, 5, 2, 5, 1, 3, 3, 3, 3, 3]
+        scores: [1, 5, 2, 5, 1, 1, 2, 3, 2, 1]
     }, 
-    {
-        name: "Accenture", 
-        photo: "accenture.com", 
-        scores: [2, 5, 3, 5, 1, 3, 3, 3, 3, 3]
-    }
+    // {
+    //     name: "Accenture", 
+    //     photo: "accenture.com", 
+    //     scores: [2, 5, 3, 5, 1, 1, 2, 3, 3, 2]
+    // }
 ]
 
 module.exports = companyArray;
