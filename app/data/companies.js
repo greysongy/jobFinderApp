e3339d454003696a13116d32517374d5aad89699
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-01 22:45:55 -0700

    Tweaked UI + additional backend info about companies

--- app/data/companies.js
+++ app/data/companies.js
@@ -1,29 +1,79 @@
 var companyArray = [
     {
         name: "Google", 
-        photo: "something", 
-        scores: [2, 5, 5, 1, , 5, 4, 3, 2, 1]
+        photo: "google.com", 
+        scores: [2, 5, 5, 1, 1, 3, 4, 2, 5, 2]
     }, 
     {
         name: "Facebook", 
-        photo: "something", 
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        photo: "facebook.com", 
+        scores: [2, 5, 5, 3, 1, 3, 4, 1, 4, 2]
     }, 
     {
         name: "Apple",
-        photo: "something", 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
+        photo: "apple.com", 
+        scores: [1, 5, 5, 2, 1, 3, 3, 3, 3, 3] 
 
     }, 
     {
         name: "Amazon", 
-        photo: "", 
-        scores: []
+        photo: "amazon.com", 
+        scores: [1, 5, 5, 3, 1, 3, 3, 3, 3, 3]
     }, 
     {
         name: "Microsoft", 
-        photo: "", 
-        scores: []
+        photo: "microsoft.com", 
+        scores: [2, 5, 5, 2, 2, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "ACLU", 
+        photo: "aclu.com", 
+        scores: [1, 5, 2, 2, 5, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Electronic Frontier Foundation", 
+        photo: "eff.org", 
+        scores: [1, 3, 5, 2, 5, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Kiva", 
+        photo: "kiva.org", 
+        scores: [3, 3, 4, 5, 5, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Earth Justice", 
+        photo: "earthjustice.org", 
+        scores: [1, 3, 3, 2, 5, 3, 3, 3, 3, 3] 
+    }, 
+    {
+        name: "Brex", 
+        photo: "brex.com", 
+        scores: [5, 3, 5, 5, 1, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Superhuman", 
+        photo: "superhuman.com", 
+        scores: [5, 2, 5, 3, 1, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "McKinsey", 
+        photo: "mckinsey.com", 
+        scores: [2, 5, 3, 5, 1, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Goldman Sachs", 
+        photo: "goldmansachs.com", 
+        scores: [1, 5, 3, 5, 1, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Morgan Stanley", 
+        photo: "morganstanley.com", 
+        scores: [1, 5, 2, 5, 1, 3, 3, 3, 3, 3]
+    }, 
+    {
+        name: "Accenture", 
+        photo: "accenture.com", 
+        scores: [2, 5, 3, 5, 1, 3, 3, 3, 3, 3]
     }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,12 +17,20 @@ module.exports = function(app) {
     app.post("/api/people", function(req, res) {
         // console.log("Request Body");
         // console.log(req.body);
+        peopleData = [];
         peopleData.push(req.body);
         var companyMatch;
         var topCompanyScore = Infinity;
         for(var i = 0; i < companyData.length; i++) {
             var totalDifference = 0;
+            // console.log("People Data");
+            //     console.log(peopleData);
+            //     console.log("Company Data");
+            //     console.log(companyData);
+                console.log("i " + i);
+                console.log(companyData[i]);
             for(var j = 0; j < 10; j++) {
+                console.log("j " + j);
                 totalDifference += Math.abs(companyData[i].scores[j] - peopleData[0].scores[j]);
             }
             if(totalDifference < topCompanyScore) {
@@ -32,7 +40,9 @@ module.exports = function(app) {
         }
         console.log("Company Match");
         console.log(companyMatch);
-        res.json(true);
+        // console.log("Response");
+        // console.log(res);
+        res.json(companyMatch);
         //do I need something with res.json
     });
 
